8659e25b0addd42c1455a18cbf1dcfe3bf6442f7
flowerinthenight <root@flowerinthenight.com>
2024-05-30 21:34:59 +0900

    chore: prioritize reading smaps_rollup if possible

--- memx.go
+++ memx.go
@@ -8,8 +8,38 @@ import (
 	"strings"
 )
 
-// GetMemoryUsage returns the calling process' internal + shared memory usage in kB.
+// Faster version. Prioritize in call.
+func getMemRollup() (uint64, error) {
+	f, err := os.Open(fmt.Sprintf("/proc/%d/smaps_rollup", os.Getpid()))
+	if err != nil {
+		return 0, err
+	}
+
+	defer f.Close()
+	r := bufio.NewScanner(f)
+	for r.Scan() {
+		line := r.Bytes()
+		if bytes.HasPrefix(line, []byte("Pss:")) {
+			var mem uint64
+			_, err := fmt.Sscanf(string(line[4:]), "%d", &mem)
+			if err != nil {
+				return 0, err
+			}
+
+			return mem, nil
+		}
+	}
+
+	return 0, fmt.Errorf("not found")
+}
+
+// GetMemoryUsage returns the calling process' private + shared memory usage in kB.
 func GetMemoryUsage() (uint64, error) {
+	memr, err := getMemRollup()
+	if err == nil {
+		return memr, nil
+	}
+
 	f, err := os.Open(fmt.Sprintf("/proc/%d/smaps", os.Getpid()))
 	if err != nil {
 		return 0, err
